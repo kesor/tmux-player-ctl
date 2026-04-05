5ad281c38ae40431d4130d3593ecabe6a3f50fa9
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-04-05 05:17:06 +0300

    back to clean display test

--- test_display.py
+++ test_display.py
@@ -12,14 +12,14 @@ tpc.s.available_players = ["spotifyd", "firefox"]
 
 # Ruler
 print("1234567890" * 10)
-tpc.s.state.position = 45000
-tpc.s.state.length = 140000
+tpc.s.state.position = 45
+tpc.s.state.length = 140
 tpc.s.state.volume = 89
-tpc.s.state.player = "spotifydIyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoiIiJTaW1wbGUgZGlzcGxheSB0ZXN0IGZvciBwcm9ncmVz"
+tpc.s.state.player = "spotifyd"
 tpc.s.state.status = "Playing"
-tpc.s.state.title = "Mission SelectIyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoiIiJTaW1wbGUgZGlzcGxheSB0ZXN0IGZvciBwcm9ncmVz"
-tpc.s.state.artist = "Matthew S BurnsIyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoiIiJTaW1wbGUgZGlzcGxheSB0ZXN0IGZvciBwcm9ncmVz"
-tpc.s.state.album = "Möbius Front '83 (Original Soundtrack)IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwoiIiJTaW1wbGUgZGlzcGxheSB0ZXN0IGZvciBwcm9ncmVz"
+tpc.s.state.title = "Mission Select"
+tpc.s.state.artist = "Matthew S Burns"
+tpc.s.state.album = "Möbius Front '83 (Original Soundtrack)"
 print(tpc.border_top())
 print(tpc.header_row())
 print(tpc.border_mid())
